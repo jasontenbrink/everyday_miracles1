8add17e6f807e1613d1f27c0973936abf6bb5b9f
jdrew5 <jdrew5@hotmail.com>
2015-12-17 15:20:11 -0600

    commit before pull

--- client/scripts/controllers/attendanceController.js
+++ client/scripts/controllers/attendanceController.js
@@ -1,5 +1,8 @@
 app.controller('AttendanceController',['$scope', '$http', 'RegisterForClassFactory', function ($scope, $http, RegisterForClassFactory) {
 
+    $scope.userEventSchedule = [];
+    $scope.event = [];
+
     $scope.registerForClassFactory = RegisterForClassFactory;
 
     //get event info from the registerForClassFactory
@@ -11,11 +14,22 @@ app.controller('AttendanceController',['$scope', '$http', 'RegisterForClassFacto
     $scope.eventId = $scope.eventFromFactory.eventId;
     $scope.eventScheduleId = $scope.eventFromFactory.eventScheduleId;
 
+    var event2 = {eventId: 1,
+        eventScheduleId: 1};
+
+    console.log("Input to get /event/byEventIdEventScheduleId ", event2);
+    $http.get('/event/byEventIdEventScheduleId', {params: event2}).then(function(response){
+        console.log("Output from get /event/byEventIdEventScheduleId ", response.data);
+        $scope.event = response.data;
+    });
+
     // get data from the database
-    var eventSchedule = {eventScheduleId: $scope.eventScheduleId};
+    //var eventSchedule = {eventScheduleId: $scope.eventScheduleId};
+    var eventSchedule = {eventScheduleId: 1};
     console.log("Input to get /usersEventSchedule/byEventScheduleId ", eventSchedule);
     $http.get('/usersEventSchedule/byEventScheduleId', {params: eventSchedule}).then(function(response){
         console.log("Output from get /usersEventSchedule/byEventScheduleId ", response.data);
+        $scope.userEventSchedule = response.data;
     });
 
 }]);
