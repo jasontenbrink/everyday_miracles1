a129bded07bef054c24a41126d242de28422fee5
Paul Zimmel <pazimmel@gmail.com>
2015-12-17 13:49:25 -0600

    save profile works

--- client/scripts/controllers/profileController.js
+++ client/scripts/controllers/profileController.js
@@ -3,13 +3,14 @@ app.controller("ProfileController", ["$scope", "$http", function($scope, $http){
     $scope.tempUser = {};
 
     //test user data to populate form
-    var user = {
+    var testUser = {
         userId: 1
     };
 
     //get profile info for profile page
-    $scope.getUser = function(user){
-        $http.get('/users/byUserId', {params: user}).then(function (response) {
+    $scope.getUser = function(someuser){
+        console.log("the input of getUser: ",someuser);
+        $http.get('/users/byUserId', {params: someuser}).then(function (response) {
             console.log("Output from get /users/byUserId ", response.data);
             $scope.tempUser = response.data[0];
 
@@ -19,20 +20,27 @@ app.controller("ProfileController", ["$scope", "$http", function($scope, $http){
             $scope.user.userId = $scope.tempUser.user_id;
             $scope.user.userName = $scope.tempUser.user_name;
             $scope.user.password = $scope.tempUser.password;
-            $scope.user.role = $scope.tempUser.role_name;
+            $scope.user.roleName = $scope.tempUser.role_name;
             $scope.user.dateOfBirth = $scope.tempUser.date_of_birth;
             $scope.user.phoneNumber = $scope.tempUser.phone_number;
             $scope.user.emailAdress = $scope.tempUser.email_address;
             $scope.user.contactType = $scope.tempUser.contact_type;
             $scope.user.paymentType = $scope.tempUser.payment_type;
-            $scope.user.everydayMiraclesClientInd = $scope.everyday_miracles_client_ind;
+            $scope.user.everydayMiraclesClientInd = $scope.tempUser.everyday_miracles_client_ind;
             $scope.user.doulaName = $scope.tempUser.doula_name;
             $scope.user.expectedBirthDate = $scope.tempUser.expected_birth_date;
 
-            console.log("the #scope.user: ", user);
+            console.log("the #scope.user: ", $scope.user);
         });
     };
 
-    $scope.getUser(user);
+    $scope.saveProfile = function(someuser) {
+
+        console.log("Input to put /users ", someuser);
+        $http.put('/users', someuser).then(function (response) {
+            console.log("Output from put /users ", response.data);
+        });
+    };
+    $scope.getUser(testUser);
 
 }]);
