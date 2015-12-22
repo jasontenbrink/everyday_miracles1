8fb5f08d0f76a59b3aa7404751451c693ad55172
jasontenbrink <jason.tenbrink@gmail.com>
2015-12-22 09:35:55 -0600

    another commit

--- server/strategies/localStrategy.js
+++ server/strategies/localStrategy.js
@@ -60,8 +60,7 @@ passport.use('local', new localStrategy({
 
     //don't add in 'done' as the third parameter, it will eat the 'done' that
     //the callback strategy needs.
-    pg.connect(connectionString, function (err,client) {
-      console.log('client: ', client);    
+    pg.connect(connectionString, function (err,client) {  
       //get hashed password to compare
       client.query("select password from users where user_name = $1", [req.body.username],
       function (err, response) {
