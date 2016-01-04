f73b69a82073d333fff91e680505f320539d5a2b
Paul Zimmel <pazimmel@gmail.com>
2016-01-04 16:57:18 -0600

    getting ready for a pull

--- server/routes/jadeTest.js
+++ server/routes/jadeTest.js
@@ -11,7 +11,7 @@ router.get('/:name?', function (req, res) {
     res.render('templates/' + name, {message: 'Hello there!'});
   }
   else{
-    console.log("req.user: ", req.user)
+    console.log("req.user: ", req.user);
     res.render('routes/' + name, {user: req.user});
   }
 });
