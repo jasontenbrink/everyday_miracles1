8f50934ac9aa608bb937036d36e63fba4efdd188
Paul Zimmel <pazimmel@gmail.com>
2016-01-04 14:59:52 -0600

    event clicked works

--- server/public/assets/scripts/controllers.js
+++ server/public/assets/scripts/controllers.js
@@ -336,7 +336,6 @@ app.controller('ChooseClassDatesController',['$scope', '$http', "RegisterForClas
 
   $scope.user = {};
   $scope.today = new Date();
-  console.log("today!!!  ",$scope.today);
   //test user info
   $scope.user.userId = 1;
 
@@ -369,6 +368,7 @@ app.controller('ChooseClassDatesController',['$scope', '$http', "RegisterForClas
 
   //get all class instances for this particular class
   $scope.loadEventData =  function(event) {
+    console.log("the event from factory: ", event);
     var eventId = {
       eventId: event.eventId
     };
@@ -390,6 +390,10 @@ app.controller('ChooseClassDatesController',['$scope', '$http', "RegisterForClas
         if ($scope.registeredEvents[i].event_schedule_id == $scope.event[j].event_schedule_id) {
             $scope.event[j].addCheckbox = true;
             //console.log("true");
+        }
+        if ($scope.event[j].event_schedule_id == $scope.eventFromFactory.eventScheduleId) {
+          console.log("making sure the event clicked on is checked");
+          $scope.event[j].addCheckbox = true;
         }
       }
     }
@@ -631,25 +635,7 @@ app.controller('EventDetailsController',['$scope', '$http', "RegisterForClassFac
   };
 
   $scope.getEventDetails($scope.eventFromFactory);
-  //sample data in eventDate
-  //can change it to be something else
-  //$scope.event = {
-  //  start:"2015-12-05T14:00:00Z",
-  //  end:"2015-12-05T15:00:00Z",
-  //  title: "Mom-to-Mom Group",
-  //  description: "Need a moment to stop, connect, and breathe? Free and on-going for expecting, new, and experienced mothers. This group, put on by The Nursing Nook, offers gentle yoga and meditation, mother-to-mother support, local breastfeeding resources, playdate for breastfeeding babes, monthly guest speakers. Childcare for older siblings may be available. Please pre-register."
-  //};
 
-  //need the click stuff
-
-  //get the event stuff from the server
-
-
-  // $http.get('/jade')
-  //   .then(function (response) {
-  //     console.log(response.data);
-  //     $scope.y = response.data;
-  //   });
 
 }]);
 
