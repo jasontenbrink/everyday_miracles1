1728df800958dfe04e38d3f868e6d846912e264c
jasontenbrink <jason.tenbrink@gmail.com>
2016-01-07 21:26:02 -0600

    admin can register a new student

--- server/routes/userRegistration.js
+++ server/routes/userRegistration.js
@@ -14,8 +14,14 @@ var connectionString = process.env.DATABASE_URL   || 'postgres://localhost:5432/
 //     res.sendFile(path.resolve(__dirname, '../public/views/register.html'));
 // });
 
+
+/*jshint multistr: true */
 router.post('/', function(req,res,next){
   var user = req.body;
+  var roleId = 1;
+  if (req.body !== ""){
+    roleId = req.body.roleId;
+  }
   console.log('req.body in post', req.body);
 
 //  if(!user.isModified('password')) return next;
@@ -28,14 +34,45 @@ router.post('/', function(req,res,next){
         return bcrypt.hashAsync(user.password, salt);
       })
       .then(function(hash){
-         user.password = hash;
+         req.body.password = hash;
             console.log('pwd from inside bcrypt after hash', user.password);
             //next();
+
             pg.connect(connectionString, function (err, client, done) {
               if (err) console.log(err);
               console.log('pwd from just before DB write req', req.body.password);
-              client.query('insert into users (user_name, password, first_name, last_name, role_id) VALUES ($1, $2, $3, $4, 1)',
-                  [req.body.username, req.body.password, req.body.firstName, req.body.lastName],
+
+              client.query("INSERT INTO users (user_name, \
+                  password, \
+                  first_name, \
+                  last_name, \
+                  role_id, \
+                  date_of_birth, \
+                  phone_number, \
+                  email_address, \
+                  contact_type, \
+                  payment_type, \
+                  everyday_miracles_client_ind, \
+                  doula_name, \
+                  expected_birth_date) \
+              VALUES \
+              ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11, $12, $13) \
+              RETURNING user_id;",
+                  [req.body.username,
+                  req.body.password,
+                  req.body.firstName,
+                  req.body.lastName,
+                  roleId,
+                  req.body.dateOfBirth,
+                  req.body.phoneNumber,
+                  req.body.emailAddress,
+                  req.body.contactType,
+                  req.body.paymentType,
+                  req.body.everydayMiraclesClientInd,
+                  req.body.doulaName,
+                  req.body.expectedBirthDate],
+
+
                   function (err, res) {
                     if (err) console.log(err);
 
