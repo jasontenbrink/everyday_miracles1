a9afd2bdd6a59afe952d8f6eacfa688a3d43760b
jasontenbrink <jason.tenbrink@gmail.com>
2016-01-14 10:15:16 -0600

    taking out console logs

--- server/routes/changePassword.js
+++ server/routes/changePassword.js
@@ -18,7 +18,7 @@ var connectionString = process.env.DATABASE_URL   || 'postgres://localhost:5432/
 // });
 
 router.put('/', userLogic, function(req,res,next){
-  console.log('req.targetUser', req.targetUser);
+
 
   //target user gets attached to the req in the userLogic middleware
   var user1 = {};
@@ -26,21 +26,20 @@ router.put('/', userLogic, function(req,res,next){
               username: req.targetUser.params.userName,
               userId: req.targetUser.params.userId
             };
-  console.log('user1', user1);
+
 
     bcrypt.genSaltAsync(SALT_WORK_FACTOR).then(function(salt){
       //  if(err) return next(err);
-        console.log('value of salt, before hash', salt);
-        console.log('value of pwd before hash', user1.password);
+
         return bcrypt.hashAsync(user1.password, salt);
       })
       .then(function(hash){
          user1.password = hash;
-            console.log('pwd from inside bcrypt after hash', user1.password);
+
             //next();
             pg.connect(connectionString, function (err, client, done) {
               if (err) console.log(err);
-              console.log('pwd from just before DB write', user1);
+
               client.query("UPDATE users \
                             SET password = $1 \
                             WHERE user_id = $2;",
